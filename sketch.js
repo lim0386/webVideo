bc7a4d69e6bb720e71a327543929ce93084d1cb1
lim0386 <lim0386@Yangui-Mac-mini.local>
2018-05-01 10:49:45 +0900

    webGL test
    testing

--- sketch.js
+++ sketch.js
@@ -1,26 +1,24 @@
 /*
- * @name Video Capture
- * @frame 710,240
- * @description <p><em><span class="small"> To run this example locally, you will need the
- * <a href="http://p5js.org/reference/#/libraries/p5.dom">p5.dom library</a>
- * at least one video file, and a running <a href="https://github.com/processing/p5.js/wiki/Local-server">local server</a>.</span></em></p><br><br>
- * Capture video from the webcam and display
- * on the canvas as well with invert filter. Note that by
- * default the capture feed shows up, too. You can hide the
- * feed by uncommenting the capture.hide() line.
+ * @name Sine Cosine in 3D
+ * @description Sine, cosine and push / pop could be applied in 3D as well.
  */
-var capture;
-
-function setup() {
-  createCanvas(390, 240);
-  capture = createCapture(VIDEO);
-  capture.size(320, 240);
-  //capture.hide();
+function setup(){
+  createCanvas(710, 400, WEBGL);
 }
 
-function draw() {
-ellipse(0, 0, 50, 50);
-  background(255);
-  image(capture, 0, 0, 320, 240);
-  filter('INVERT');
+function draw(){
+  background(250);
+  rotateY(frameCount * 0.01);
+
+  for(var j = 0; j < 5; j++){
+    push();
+    for(var i = 0; i < 80; i++){
+      translate(sin(frameCount * 0.001 + j) * 100, sin(frameCount * 0.001 + j) * 100, i * 0.1);
+      rotateZ(frameCount * 0.002);
+      push();
+      sphere(8, 6, 4); 
+      pop();
+    }
+    pop();
+  }
 }
